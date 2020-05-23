8c47b4eba00581b85afb97501517d115f7442c89
gauransh7 <gauransh7dingwani@gmail.com>
2020-05-23 22:56:44 +0530

    Updates

---
+++ precache-manifest.f469d7592d6ffcf1df4c5fe091eb8490.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f42fb492a6300e32fbdcfed410288442",
+    "url": "/tambola/index.html"
+  },
+  {
+    "revision": "5315390eccb0235e6a90",
+    "url": "/tambola/static/css/main.75cfbf6a.chunk.css"
+  },
+  {
+    "revision": "c1ed917c106b0df2e887",
+    "url": "/tambola/static/js/2.0f9d687a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tambola/static/js/2.0f9d687a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5315390eccb0235e6a90",
+    "url": "/tambola/static/js/main.6e2fe95b.chunk.js"
+  },
+  {
+    "revision": "492532a8aa08a5048166",
+    "url": "/tambola/static/js/runtime-main.b341880a.js"
+  },
+  {
+    "revision": "d4c8498044454911a9d809d6de5d7520",
+    "url": "/tambola/static/media/lahs.d4c84980.jpeg"
+  }
+]);
